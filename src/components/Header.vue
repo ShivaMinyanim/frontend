bbeaaaa87cb50296dd646d8a06606ae96f9ea90e
Ari Miller <arimiller92@gmail.com>
2017-04-16 16:54:22 -0400

    refactoring html

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="header">
+    <header class="header">
         <div class="container">
-            <div class="links">
+            <nav class="links">
                 <div class="links--left">
                     <router-link to="/next-minyanim" class="uppercase text--color-secondary">Minyanim</router-link>
                     <router-link to="/shiva-houses" class="uppercase text--color-secondary">Houses</router-link>
@@ -10,7 +10,7 @@
                     <router-link to="/donate" class="uppercase text--color-accent">Donate</router-link>
                     <router-link to="/account" class="text--color-secondary carrot--down---after">Ari Miller</router-link>
                 </div>
-            </div>
+            </nav>
             <div class="title--container">
                 <div class="logo--container">
                     <img src="../assets/logo.svg" class="logo">
@@ -18,7 +18,7 @@
                 <h1 class="title">Next Minyanim</h1>
             </div>
         </div>
-    </div>
+    </header>
 </template>
 
 <script>
--- src/components/NextMinyanim.vue
+++ src/components/NextMinyanim.vue
@@ -1,35 +1,31 @@
 <template>
-    <div class="container">
-        <div class="content-center">
-            <section>
-                <ul class="minyan-list">
-                    <li class="minyan-list--item">
-                        <div class="minyan-description">
-                            <h3>Shacharis</h3>
-                            <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
-                        </div>
-                        <div class="minyan-time">
-                            <h3>7:30 AM</h3>
-                        </div>
-                        <div class="minyan-action">
-                            <a class="button button--primary uppercase">Attend</a>
-                        </div>
-                    </li>
-                    <li class="minyan-list--item">
-                        <div class="minyan-description">
-                            <h3>Shacharis</h3>
-                            <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
-                        </div>
-                        <div class="minyan-time">
-                            <h3>7:30 AM</h3>
-                        </div>
-                        <div class="minyan-action">
-                            <a class="button button--primary uppercase">Attend</a>
-                        </div>
-                    </li>
-                </ul>
-            </section>
-        </div>
+    <div class="container content-center">
+        <section class="minyan-list">
+            <article class="minyan-list--item">
+                <div class="minyan-description">
+                    <h3>Shacharis</h3>
+                    <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
+                </div>
+                <div class="minyan-time">
+                    <h3>7:30 AM</h3>
+                </div>
+                <div class="minyan-action">
+                    <a class="button button--primary uppercase">Attend</a>
+                </div>
+            </article>
+            <article class="minyan-list--item">
+                <div class="minyan-description">
+                    <h3>Shacharis</h3>
+                    <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
+                </div>
+                <div class="minyan-time">
+                    <h3>7:30 AM</h3>
+                </div>
+                <div class="minyan-action">
+                    <a class="button button--primary uppercase">Attend</a>
+                </div>
+            </article>
+        </section>
     </div>
 </template>
 
