1f6aac169efc54ab7b96fec97a0cd202985c053c
Ari Miller <arimiller92@gmail.com>
2017-04-23 12:23:31 -0400

    add time pipe

--- src/components/NextMinyanim.vue
+++ src/components/NextMinyanim.vue
@@ -1,25 +1,13 @@
 <template>
     <div class="container center">
         <section class="minyan-list">
-            <article class="minyan-list--item">
+            <article class="minyan-list--item" v-for="minyan in minyanim">
                 <div class="minyan-description">
-                    <h3>Shacharis</h3>
-                    <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
+                    <h3>{{ minyan.type }}</h3>
+                    <span class="secondary text--color-secondary">{{ minyan.house.street }} {{ minyan.house.city }}, {{minyan.house.state }}</span>
                 </div>
                 <div class="minyan-time">
-                    <h3>7:30 AM</h3>
-                </div>
-                <div class="minyan-action">
-                    <a class="button button--primary uppercase">Attend</a>
-                </div>
-            </article>
-            <article class="minyan-list--item">
-                <div class="minyan-description">
-                    <h3>Shacharis</h3>
-                    <span class="secondary text--color-secondary">123 Main St. Teaneck, NJ</span>
-                </div>
-                <div class="minyan-time">
-                    <h3>7:30 AM</h3>
+                    <h3>{{ minyan.timestamp | time }}</h3>
                 </div>
                 <div class="minyan-action">
                     <a class="button button--primary uppercase">Attend</a>
@@ -30,11 +18,38 @@
 </template>
 
 <script>
+
+import time from '../mixins/timeFilter'
+
 export default {
     name: 'next',
+
+    mixins: [time],
+
     data () {
         return {
-            //
+            minyanim: [
+                {
+                    type: 'Shacharis',
+                    house: {
+                        street: '123 Main St.',
+                        city: 'Teaneck',
+                        state: 'NJ',
+                        zip: '07666'
+                    },
+                    timestamp: '2016-12-27 07:30:00'
+                },
+                {
+                    type: 'Mincha',
+                    house: {
+                        street: '123 Main St.',
+                        city: 'Teaneck',
+                        state: 'NJ',
+                        zip: '07666'
+                    },
+                    timestamp: '2016-12-27 16:00:00'
+                }
+            ]
         }
     }
 }
