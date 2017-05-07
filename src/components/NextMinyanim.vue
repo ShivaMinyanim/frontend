110c3eae7a899cc0a4f9e0a3e54a77e34acbafd7
Ari Miller <arimiller92@gmail.com>
2017-05-07 14:07:13 -0400

    try sub heading

--- src/components/NextMinyanim.vue
+++ src/components/NextMinyanim.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="container center">
-        <section class="minyan-list">
-            <h2>
-                <i class="fa fa-chevron-left"></i>Sunday May 7, 2017 -- י״א אייר תשע״ז<i class="fa fa-chevron-right"></i>
-            </h2>
-            <article class="minyan-list--item" v-for="minyan in minyanim">
-                <div class="minyan-description">
-                    <h3>{{ minyan.type }}</h3>
-                    <span class="secondary text--color-secondary">{{ minyan.house.street }} {{ minyan.house.city }}, {{minyan.house.state }}</span>
-                </div>
-                <div class="minyan-time">
-                    <h3>{{ minyan.timestamp | time }}</h3>
-                </div>
-                <div class="minyan-action">
-                    <a class="button button--primary uppercase">Attend</a>
-                </div>
-            </article>
-        </section>
+    <div>
+        <div class="sub-heading">
+            <i class="fa fa-chevron-left"></i>Sunday May 7, 2017 -- י״א אייר תשע״ז<i class="fa fa-chevron-right"></i>
+        </div>
+        <div class="container center">
+            <section class="minyan-list">
+                <article class="minyan-list--item" v-for="minyan in minyanim">
+                    <div class="minyan-description">
+                        <h3>{{ minyan.type }}</h3>
+                        <span class="secondary text--color-secondary">{{ minyan.house.street }} {{ minyan.house.city }}, {{minyan.house.state }}</span>
+                    </div>
+                    <div class="minyan-time">
+                        <h3>{{ minyan.timestamp | time }}</h3>
+                    </div>
+                    <div class="minyan-action">
+                        <a class="button button--primary uppercase">Attend</a>
+                    </div>
+                </article>
+            </section>
+        </div>
     </div>
 </template>
 
