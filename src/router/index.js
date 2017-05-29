44c157f29f7a528388f59a3b26078bd6a177a1bb
Ari Miller <arimiller92@gmail.com>
2017-05-28 22:49:58 -0400

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/views/Hello'
-import NextMinyanim from '@/views/NextMinyanim'
+import routes from './routes'
 
+/**
+ * Register VueRouter with our application.
+ */
 Vue.use(Router)
 
+/**
+ * Create the instance of VueRouter we
+ * will be using in our application.
+ */
 export default new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            component: Hello
-        },
-        {
-            path: '/next-minyanim',
-            component: NextMinyanim
-        }
-    ]
+    routes,
+    mode: 'history'
 })
--- src/views/NextMinyanim.vue
+++ src/views/NextMinyanim.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="sub-heading">
-            <i class="fa fa-chevron-left"></i>Sunday May 7, 2017 -- י״א אייר תשע״ז<i class="fa fa-chevron-right"></i>
+            <i class="fa fa-chevron-left" @click="previous()"></i>
+                {{ secularDate }} -- {{ hebrewDate }}
+            <i class="fa fa-chevron-right" @click="next()"></i>
         </div>
         <div class="container center">
             <section class="minyan-list">
@@ -24,6 +26,9 @@
 
 <script>
 
+import moment from 'moment'
+import axios from 'axios'
+
 import time from '@/mixins/timeFilter'
 import MinyanHttpService from '@/http/services/MinyanHttpService'
 
@@ -32,12 +37,42 @@ export default {
 
     data () {
         return {
-            minyanim: []
+            minyanim: [],
+
+            secularDate: moment().format('MMM D, YYYY'),
+            hebrewDate: ''
+        }
+    },
+
+    methods: {
+        next () {
+            // display the next days minyanim
+        },
+
+        previous () {
+            // display the previous days minyanim
         }
     },
 
     mounted () {
-        MinyanHttpService.get({ year: '2017' }).then(minyanim => this.minyanim = minyanim)
+        // load all minyanim for this week
+        // filter minyanim by today
+        const filter = { year: '2017', month: '5', day: '14' }
+        MinyanHttpService.get(filter).then(minyanim => this.minyanim = minyanim)
+
+        // MinyanHttpService.getByDate(today).then(minyanim => this.minyanim = minyanim)
+
+        let year = moment().format('YYYY')
+        let month = moment().format('M')
+        let day = moment().format('D')
+
+        // zman(today).hebrew().format('M D, Y')
+
+        axios.get(`http://www.hebcal.com/converter/?cfg=json&gy=${year}&gm=${month}&gd=${day}&g2h=2`).then(response => {
+            let date = response.data
+            // this.hebrewDate = `${date.hd} ${date.hm}, ${date.hy}`
+            this.hebrewDate = date.hebrew
+        })
     }
 }
 </script>
