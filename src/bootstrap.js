b9c34d58841f9e317d3212e3ca72bca92a206846
Ari Miller <arimiller92@gmail.com>
2017-06-04 18:29:02 -0400

    clean up bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,27 +3,19 @@ import App from '@/App'
 import router from '@/router'
 import * as filters from '@/util/filters'
 
+// set the production tip
 Vue.config.productionTip = false
 
-// register global utility filters.
+// register global utility filters
 Object.keys(filters).forEach(key => {
     Vue.filter(key, filters[key])
 })
 
-/*
-|--------------------------------------------------------------------------
-| Mount The Application
-|--------------------------------------------------------------------------
-|
-| Finally, we will create a fresh Vue component which will serve as the
-| application instance and we will mount it to the DOM. All future
-| components we create will be children of this root instance.
-|
-*/
-
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
+// create the app instance
+const app = new Vue({
     router,
     render: h => h(App)
 })
+
+// mount the application
+app.$mount('#app')
