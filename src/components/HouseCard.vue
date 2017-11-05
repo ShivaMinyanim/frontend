fceb48feeda6039a58c666a1e5328b07963646a3
Ari Miller <arimiller92@gmail.com>
2017-11-05 17:33:15 -0500

    add minyan details to house card

--- src/components/HouseCard.vue
+++ src/components/HouseCard.vue
@@ -2,6 +2,12 @@
     <section class="card">
         <h2>{{ name }}</h2>
         <h5 class="text--color-secondary">{{ address }}</h5>
+        <div class="details">
+            <div v-for="minyan in orderedMinyanim" class="text--color-secondary row">
+                <span class="capitalize">{{ minyan.type }}</span>
+                <span>{{ minyan.timestamp | time }}</span>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -11,6 +17,12 @@
             house: Object
         },
 
+        data () {
+            return {
+                minyanim: this.house.minyanim
+            }
+        },
+
         computed: {
             name () {
                 return this.house.residence_name
@@ -18,11 +30,11 @@
 
             address () {
                 return `${this.house.street} ${this.house.city}, ${this.house.state}`
-            }
-        },
+            },
 
-        mounted () {
-            console.log(this.house)
+            orderedMinyanim () {
+                return this.minyanim.sort((a, b) => a.timestamp < b.timestamp ? -1 : 1)
+            }
         }
     }
 </script>
@@ -32,10 +44,19 @@
         background white
         border-radius 4px
         box-shadow 0 -1px 1px 0 rgba(0, 0, 0, .05), 0 1px 2px 0 rgba(0, 0, 0, .2)
-        height 300px
+        height 200px
         padding 20px
         width 350px
 
         h2, h5
             margin 0
+
+    .details
+        margin-top 20px
+
+    .row
+        display flex
+        justify-content space-between
+        font-size 1.1em
+        line-height 1.6em
 </style>
